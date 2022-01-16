09d1c7d7b1d9bf00eb85ff3e21b3b533903eee81
Hasan097 <hpacific110@gmail.com>
2022-01-16 17:01:01 -0600

    First commit

---
+++ CityGuide/Routing.swift
@@ -0,0 +1,241 @@
+//
+//  BeaconCode.swift
+//  CityGuide
+//
+//  Created by Studentadm on 10/16/21.
+//
+
+import Foundation
+import CoreLocation
+
+// Matrix content: Node, Threshold, NSEW beacons, and distances.
+var matrix : [String:Int] = [
+    "node" : -1,
+    "threshold" : -1,
+    "beast" : -10,           //E
+    "edist" : -10,
+    "bneast" : -10,          //NE
+    "neastdist" : -10,
+    "bnwest" : -10,          //NW
+    "nwestdist" : -10,
+    "bseast" : -10,          //SE
+    "seastdist" : -10,
+    "bsouth" : -10,          //S
+    "sdist" : -10,
+    "bswest" : -10,          //SW
+    "swestdist" : -10,
+    "bwest" : -10,           // W
+    "wdist" : -10,
+    "bnorth" : -10,          // N
+    "ndist" : -10
+]
+var matrixDictionary : [Int:Any] = [:]
+var current = -1
+var shortestPath : [Int] = []
+var pathFound = false
+
+func groupChangeNoticed(){
+    matrixDictionary.removeAll()
+}
+
+func makeMatrix(template : [String:Any]){
+    var numsens = -1
+    if !template.isEmpty{
+        let keys = matrix.keys
+        for i in keys{
+            let val = Int(truncating: template[i] as! NSNumber)
+            matrix[i] = Int(val)
+        }
+        numsens = Int(truncating: template["numsens"] as! NSNumber)
+        if matrixDictionary.isEmpty{
+            for i in 0...numsens{
+                matrixDictionary[i] = [:]
+            }
+        }
+    }
+    
+
+    if matrix["node"] != -1 && !matrix.isEmpty{
+        let nodeVal = Int(truncating: matrix["node"] as! NSNumber)
+        matrixDictionary[nodeVal] = matrix
+    }
+
+}
+
+func pathFinder(current : Int, destination : Int) -> [Int]{
+    var visited : [Int] = []            // An array of visted nodes
+    var unvisited : [Int] = []          // An array of unvisited nodes
+    var currentNode = current
+    var shortest : [Int] = []
+    var pathDictionary : [Int : [Int]] = [ : ]
+    
+    for keys in matrixDictionary.keys{
+        if let m = matrixDictionary[keys] as? [String:Int]{
+            if m.count != 0{
+                pathDictionary[keys] = [10000000000,-1]
+                unvisited.append(keys)
+            }
+        }
+    }
+    
+    unvisited = unvisited.sorted()
+    
+    pathDictionary[currentNode] = [0,-1]
+    
+//    Test to check matrix validity of matrixDictionary.
+//    var arr : [Int] = []
+//    for i in matrixDictionary.keys{
+//        arr.append(i)
+//        arr = arr.sorted()
+//    }
+//    for j in arr{
+//        print(matrixDictionary[j])
+//    }
+    
+    if currentNode == destination{
+        shortest.append(currentNode)
+        pathFound = true
+        return shortest
+    }
+    else{
+        var nextNodeArr : [Int] = []                   // Next node after setting the smallest cost
+        while !unvisited.isEmpty{
+            //print(currentNode)
+            let connections = matrixDictionary[currentNode] as! [String:Int]    // Gives us matrix
+            
+            var currentCost : Int = (pathDictionary[currentNode]![0])
+            var vertEdges : [Int:Int] = [:]
+            
+            // Had to make this manually as in a dictionary, keys are independent of each other
+            
+            vertEdges[Int(truncating: connections["beast"]! as NSNumber)] = connections["edist"]          //E
+            
+            vertEdges[Int(truncating: connections["bneast"]! as NSNumber)] = connections["neastdist"]         //NE
+            
+            vertEdges[Int(truncating: connections["bnwest"]! as NSNumber)] = connections["nwestdist"]         //NW
+            
+            vertEdges[Int(truncating: connections["bseast"]! as NSNumber)] = connections["seastdist"]         //SE
+            
+            vertEdges[Int(truncating: connections["bsouth"]! as NSNumber)] = connections["sdist"]         //S
+            
+            vertEdges[Int(truncating: connections["bswest"]! as NSNumber)] = connections["swestdist"]         //SW
+            
+            vertEdges[Int(truncating: connections["bwest"]! as NSNumber)] = connections["wdist"]          // W
+            
+            vertEdges[Int(truncating: connections["bnorth"]! as NSNumber)] = connections["ndist"]         // N
+            
+            let connKeys = vertEdges.keys   // contains keys of the above matrix
+            for i in connKeys{              // Check for the an edge
+                if i != -10 && connKeys.count != 2{
+                    // its a node
+                    if !visited.contains(i){
+                        currentCost = (pathDictionary[currentNode]![0])
+                        currentCost = currentCost + vertEdges[i]!
+                        if (pathDictionary[i]![0]) > currentCost{
+                            pathDictionary[i]![0] = currentCost
+                            pathDictionary[i]![1] = currentNode
+                            if !nextNodeArr.contains(i){
+                                nextNodeArr.append(i)
+                            }
+                        }
+                    }
+                }
+            }
+            
+            for frst in 0..<nextNodeArr.count{  // sorting based on cost
+                let n = nextNodeArr[frst]
+                let j = frst
+                for scnd in j+1..<nextNodeArr.count{
+                    let k = nextNodeArr[scnd]
+                    if pathDictionary[n]![0] >= pathDictionary[k]![0]{
+                        nextNodeArr.swapAt(frst, scnd)
+                    }
+                }
+            }
+            
+            for check in nextNodeArr{       // To remove subnodes as the next jump
+                let checkConnections = matrixDictionary[check] as! [String:Int]
+                var Edges : [Int:Int] = [:]
+                
+                Edges[Int(truncating: checkConnections["beast"]! as NSNumber)] = checkConnections["edist"]          //E
+                Edges[Int(truncating: checkConnections["bneast"]! as NSNumber)] = checkConnections["neastdist"]         //NE
+                Edges[Int(truncating: checkConnections["bnwest"]! as NSNumber)] = checkConnections["nwestdist"]         //NW
+                Edges[Int(truncating: checkConnections["bseast"]! as NSNumber)] = checkConnections["seastdist"]         //SE
+                Edges[Int(truncating: checkConnections["bsouth"]! as NSNumber)] = checkConnections["sdist"]         //S
+                Edges[Int(truncating: checkConnections["bswest"]! as NSNumber)] = checkConnections["swestdist"]         //SW
+                Edges[Int(truncating: checkConnections["bwest"]! as NSNumber)] = checkConnections["wdist"]          // W
+                Edges[Int(truncating: checkConnections["bnorth"]! as NSNumber)] = checkConnections["ndist"]         // N
+                
+                if Edges.keys.count == 2{
+                    nextNodeArr.remove(at: nextNodeArr.firstIndex(of: check)!)
+                    
+                    if !visited.contains(check) && unvisited.contains(check){
+                        visited.append(check)
+                        unvisited.remove(at: unvisited.firstIndex(of: check)!)
+                    }
+                }
+            }
+            
+            print(nextNodeArr)
+            if !nextNodeArr.isEmpty{
+                if !visited.contains(currentNode) && unvisited.contains(currentNode){
+                    visited.append(currentNode)
+                    unvisited.remove(at: unvisited.firstIndex(of: currentNode)!)
+                }
+                if nextNodeArr.contains(currentNode){
+                    nextNodeArr.remove(at: nextNodeArr.firstIndex(of: currentNode)!)
+                }
+                
+                if !nextNodeArr.isEmpty{
+                    currentNode = nextNodeArr.first!
+                }
+            }
+            else{
+                if !visited.contains(currentNode) && unvisited.contains(currentNode){
+                    visited.append(currentNode)
+                    unvisited.remove(at: unvisited.firstIndex(of: currentNode)!)
+                }
+            }
+        }
+        
+//        for k in pathDictionary.keys{
+//            let a = pathDictionary[k]
+//            print("From: " + String(current))
+//            print("To: " + String(k))
+//            print("Cost: " + String(a![0]))
+//            print("Previous Node: " + String(a![1]))
+//            print("=====================================")
+//        }
+        shortest = extractPath(matrix: pathDictionary, start: current, Finish: destination)
+        
+        pathFound = true
+        return shortest
+    }
+    
+}
+
+func extractPath(matrix : [Int : [Int]], start : Int, Finish : Int) -> [Int]{
+    var path : [Int] = []
+    var prevNode : Int = -1
+
+    for keys in matrix.keys{
+        if Finish == keys{
+            path.append(Finish)
+            prevNode = matrix[keys]![1]
+        }
+    }
+    
+    if prevNode == start{
+        path.append(prevNode)
+    }
+    else{
+        while(prevNode != start){
+            path.append(prevNode)
+            prevNode = matrix[prevNode]![1]
+        }
+        path.append(prevNode)
+    }
+    
+    path = path.reversed()
+    return path
+}
--- CityGuide/ViewController.swift
+++ CityGuide/ViewController.swift
@@ -6,14 +6,246 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var recButton: UIButton!
+    @IBOutlet weak var settingsBtn: UIButton!
+//    @IBOutlet weak var searchBar: UISearchBar!
+//    @IBOutlet weak var searchBarPosition: NSLayoutConstraint!
+    @IBOutlet weak var compassImage: UIImageView!
+    @IBOutlet weak var floorPlan: UIImageView!
+    
+    var beaconManager : CLLocationManager?
+    var locationManager : CLLocationManager?
+    var userDefinedRssi: Float = 0.0
+    var beaconList : [Int] = []
+    var detectedGroupId = -1
+    var groupID : Int = -1
+    var floorNo : Int = -10
+    var CURRENT_NODE = -1
+    var newGroupNoticed = false
+    var getBeaconsFlag = false
+    var getThePath = false
+    let srVC = SearchResultsVC()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        overrideUserInterfaceStyle = .dark // The UI is better in darkmode.
+        
+        beaconManager = CLLocationManager()
+        beaconManager?.delegate = self
+        beaconManager?.requestAlwaysAuthorization()
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.startUpdatingHeading()
+        
+        if let userInputs = UserDefaults.standard.value(forKey: "userInputItems") as? [String : Float]{
+            userDefinedRssi = userInputs["Set Threshold"] ?? (-80.00)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        UIView.animate(withDuration: 0.5) {
+            var angle = newHeading.trueHeading
+            angle = angle * .pi / 180               // to convert degrees to radians
+//            var degrees = angle * 180 / .pi         // to convert radinas to degrees
+//            print("Direction: " + String(degrees))
+            self.compassImage.transform = CGAffineTransform(rotationAngle: angle)
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus{
+            case .authorizedAlways, .authorizedWhenInUse:
+                print("Authorized")
+                if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
+                    if CLLocationManager.isRangingAvailable(){
+                        startScanning()
+                    }
+                }
+            case .notDetermined:
+                let alert = UIAlertController.init(title: "Cannot Find Beacons", message: "Permissions were undetermined.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            case .restricted:
+                let alert = UIAlertController.init(title: "Cannot Find Beacons", message: "Permissions were restricted.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            case .denied:
+                let alert = UIAlertController.init(title: "Cannot Find Beacons", message: "Permissions were denied.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            @unknown default:
+                let alert = UIAlertController.init(title: "Cannot Find Beacons", message: "Permissions were denied.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func startScanning(){
+        let uuid = UUID.init(uuidString: "CA1D78EA-BE1A-4580-8D87-1F60B67A80AB")!
+        let beaconRegion = CLBeaconRegion.init(uuid: uuid, major: 0, identifier: "Gimbal Beacon")
+        let beconIdConstraint = CLBeaconIdentityConstraint.init(uuid: uuid)
+        beaconManager?.startMonitoring(for: beaconRegion)
+        beaconManager?.startRangingBeacons(satisfying: beconIdConstraint)
+    }
+    
+    func updateBeaconReading(distance : CLProximity, beacon: CLBeacon?){
+        
+        if beacon?.minor != nil{
+            CURRENT_NODE = Int(truncating: beacon!.minor as NSNumber)
+            if beaconList.contains(Int(truncating: beacon!.minor)) == false{
+                let beaconID = Int(truncating: beacon!.minor)
+                beaconList.append(beaconID)
+                postToDB(typeOfAction: "beacons", beaconID: beaconID, auth: "eW7jYaEz7mnx0rrM", floorNum: nil, vc: self)
+            }
+        }
+        
+        if dArray.count != 0{
+            let temp = dArray[0]
+            if let val = temp["group_id"] as? Int{
+                if groupID != val{
+                    newGroupNoticed = true
+                    groupID = val
+                }
+                if detectedGroupId != groupID && detectedGroupId != -1{
+                    detectedGroupId = groupID
+                    groupChangeNoticed()                // Call to get reset matrix values
+                }
+            }
+            if let v = temp["_level"] as? Int{
+                if floorNo != v{
+                    floorNo = v
+                    postToDB(typeOfAction: "getFloor", beaconID: groupID, auth: "eW7jYaEz7mnx0rrM", floorNum: floorNo, vc: self)
+                }
+            }
+        }
+        
+        if imageView.image != nil && floorPlan.image != imageView.image{
+            self.floorPlan.image = imageView.image
+        }
+        
+        if groupID != -1 && newGroupNoticed && floorNo != -10{
+            // get all the beacons for the new group.
+            print("Group ID set: \(groupID)")
+            postToDB(typeOfAction: "getbeacons", beaconID: groupID, auth: "eW7jYaEz7mnx0rrM", floorNum: floorNo, vc: self)
+            newGroupNoticed = false
+            getBeaconsFlag = true
+        }
+        
+        if getBeaconsFlag && !listOfBecaon.isEmpty{ // get all values of the new set of beacons
+            for i in listOfBecaon{
+                if !beaconList.contains(i){
+                    beaconList.append(i)
+                    postToDB(typeOfAction: "beacons", beaconID: i, auth: "eW7jYaEz7mnx0rrM", floorNum: nil, vc: self)
+                }
+            }
+            getBeaconsFlag = false
+        }
+        
+        if destinations.count != srVC.locations.count{
+            srVC.getLocations(values: destinations)
+        }
+        
+        if pathFound{
+            print("Path Found!")
+            for i in dArray{
+                if i["beacon_id"] as! Int == CURRENT_NODE{
+                    if let checkerForHub = i["locname"] as? String{
+                        if checkerForHub.contains("Hub "){
+                            let n = i["node"] as! Int
+                            if n != shortestPath.first!{
+                                print("Re-Routing...")
+                                shortestPath = pathFinder(current: n, destination: shortestPath.last!)
+                            }
+                        }
+                    }
+                }
+            }
+            print(shortestPath)
+            pathFound = false
+        }
+        
+        switch distance {
+        case .unknown:
+            print("No Beacons Detected: ")
+        case .immediate:
+            print("Beacon is very close: " + String(describing: beacon!.minor) + " " + String(beacon!.rssi))
+        case .near:
+            print("Beacon is near: "  + String(describing: beacon!.minor) + " " + String(beacon!.rssi))
+        case .far:
+            print("Beacon is Far: "  + String(describing: beacon!.minor) + " " + String(beacon!.rssi))
+            // rssi must be less than the set threshold
+            if Float(beacon!.rssi) < userDefinedRssi{
+                // call the screening window function.
+            }
+        @unknown default:
+            print("No Beacons Detected (Default)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        var filteredBeacons : [CLBeacon] = []
+        for i in beacons{
+            if i.rssi != 0{
+                filteredBeacons.append(i)
+            }
+        }
+
+        if let beacon = filteredBeacons.first{
+            updateBeaconReading(distance: beacon.proximity, beacon: beacon)
+        }
+        else{
+            updateBeaconReading(distance: .unknown, beacon: nil)
+        }
+    }
+    
+    func presentAlert(alert : UIAlertController){
+        self.present(alert, animated: true, completion: nil)
     }
 
+    @IBAction func didTapSettingsButton(){
+        let tvc = SettingsTableController()
+        tvc.items = [
+            "User Category",
+            "Route Preview",
+            "Distance Unit",
+            "Referece Distance Unit",
+            "Orientation Preference" ,
+            "Monitoring" ,
+            "Step Size (ft)",
+            "Weighted Moving Average",
+            "Set Threshold" ,
+            "Timer (Seconds)",
+            "Searching Radius (Meters)" ,
+            "GPS Accuracy"
+        ]
+        tvc.title = "Settings"
+        
+        navigationController?.pushViewController(tvc, animated: true)
 
+    }
+    
+    
+    @IBAction func searchTapped(_ sender: Any) {
+        for i in dArray{
+            if i["beacon_id"] as! Int == CURRENT_NODE{
+                if let checkerForHub = i["locname"] as? String{
+                    if checkerForHub.contains("Hub "){
+                        let n = i["node"] as! Int
+                        srVC.setCurrentNode(node: n)
+                    }
+                }
+            }
+        }
+        navigationController?.pushViewController(srVC, animated: true)
+    }
+    
 }
+
 
